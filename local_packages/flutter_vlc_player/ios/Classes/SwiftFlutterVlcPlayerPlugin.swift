c529b66d43224943e74ebe91fdb0d2e90fff6cf9
Sagar Patel <sptl654456@gmail.com>
2023-11-05 16:43:49 +0530

    local package delete

--- local_packages/flutter_vlc_player/ios/Classes/SwiftFlutterVlcPlayerPlugin.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Flutter
-
-public class SwiftFlutterVlcPlayerPlugin: NSObject, FlutterPlugin {
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let vlcViewFactory = VLCViewFactory(registrar: registrar)
-        registrar.register(vlcViewFactory, withId: "flutter_video_plugin/getVideoView")
-    }
-}
